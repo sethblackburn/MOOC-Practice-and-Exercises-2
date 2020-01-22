0c9e0d17b5ad4cadbbb9604f6e6000b9285fd0e0
sethblackburn <56046084+sethblackburn@users.noreply.github.com>
2020-01-22 15:56:46 -0600

    Create Main.java

---
+++ 21.6 Ex73 FirstMultiObject/Main.java
@@ -0,0 +1,14 @@
+// 21.6 Ex73; https://materiaalit.github.io/2013-oo-programming/part1/week-4/#e73; 22 Jan 2020
+
+// Working with objects opens up so many possibilities. All this time I've been imagining how to develop certain ideas, but
+// my imagination was bound by what could be done in a single object.
+
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Product banana = new Product("Banana", 1.1, 13);
+
+        banana.printProduct();
+    }
+}
