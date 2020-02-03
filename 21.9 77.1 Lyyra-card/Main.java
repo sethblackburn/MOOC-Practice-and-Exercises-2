e7381e8e9dcb6432999ffc4c00fb3708388924a1
sethblackburn <56046084+sethblackburn@users.noreply.github.com>
2020-02-03 17:38:45 -0600

    Update Main.java

--- 21.9 77.1 Lyyra-card/Main.java
+++ 21.9 77.1 Lyyra-card/Main.java
@@ -1,4 +1,4 @@
-// MOOC 21.9; https://materiaalit.github.io/2013-oo-programming/part1/week-4/#e77; 03 Feb 2020
+// MOOC 21.9 Ex 77.1; https://materiaalit.github.io/2013-oo-programming/part1/week-4/#e77; 03 Feb 2020
 
 // I like toString() a lot. Anything that can be embedded and eliminates the need to have something written out multiple
 // times?  Yes. Please. And thank you.
