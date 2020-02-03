532652868fee8776c5f4cfb3409aa7b356092a97
sethblackburn <56046084+sethblackburn@users.noreply.github.com>
2020-02-03 17:38:20 -0600

    Create Main.java

---
+++ 21.9 77.1 Lyyra-card/Main.java
@@ -0,0 +1,13 @@
+// MOOC 21.9; https://materiaalit.github.io/2013-oo-programming/part1/week-4/#e77; 03 Feb 2020
+
+// I like toString() a lot. Anything that can be embedded and eliminates the need to have something written out multiple
+// times?  Yes. Please. And thank you.
+
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        LyyraCard card = new LyyraCard(50);
+        System.out.println(card);
+    }
+}
